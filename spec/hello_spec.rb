9cb53aaecea08aa6209a7fed6b0ba2701f6baef7
Anderson Ferreira <andersonfoliveira@yahoo.com>
2020-08-03 11:49:22 -0300

    configurando janela do navegador

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -1,4 +1,9 @@
 describe 'primeiro script', :helo do
+    
+    before(:each) do 
+        #ajustando resolução da tela
+        page.current_window.resize_to(1280, 800)
+    end
 
     it  'visitar página' do
         visit '/'
